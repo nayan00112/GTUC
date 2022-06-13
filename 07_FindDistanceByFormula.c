db50837d251feef13ca2ff4ed44d4396f518add9
Nayan Patel <nayanpatel00112@gmail.com>
2022-06-13 21:21:43 +0530

    C Language

--- 07_FindDistanceByFormula.c
+++ 07_FindDistanceByFormula.c
@@ -4,21 +4,33 @@ int main()
 {
     float u, t, d, a = 9.8;
 
-    printf("Enter the value of initial velocity in m/s.\n");
+    printf("Enter the value of initial velocity in m/s: ");
     scanf("%f", &u);
 
-    printf("Enter the value of time in s.\n");
+    printf("Enter the value of time in s: ");
     scanf("%f", &t);
 
     d = u * t + 0.5 * a * t * t;
 
     if (d > 1000)
     {
-        printf("Thw distance cover by the object is %fkm", d * 0.001);
+        printf("The distance cover by the object is %fkm", d * 0.001);
     }
     else
     {
-        printf("Thw distance cover by the object is %fm", d);
+        printf("The distance cover by the object is %fm", d);
     }
     return 0;
 }
+
+// Output:_________________________________________________________-
+
+/*
+Enter the value of initial velocity in m/s: 6
+Enter the value of time in s: 7
+The distance cover by the object is 282.100006m
+
+Enter the value of initial velocity in m/s: 25
+Enter the value of time in s: 125
+The distance cover by the object is 79.687500km
+*/
